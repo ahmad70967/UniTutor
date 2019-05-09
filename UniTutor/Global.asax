bedad0f1002445699f28702a4b6555a6c1848414
admin <fatima.anm@hotmail.com>
2019-05-09 19:35:15 +0200

    Add project files.

---
+++ UniTutor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UniTutor.MvcApplication" Language="C#" %>
---
+++ UniTutor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UniTutor.Startup))]
+namespace UniTutor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
